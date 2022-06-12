0dc3420a10faa02bf1af78299137d0f4514a85f5
Andrey <lee1975uz@gmail.com>
2022-06-12 13:23:16 +0900

    add /me custom url

--- src/api/event/controllers/event.js
+++ src/api/event/controllers/event.js
@@ -1,9 +1,36 @@
-'use strict';
+"use strict";
+const { sanitize } = require("@strapi/utils");
 
 /**
  *  event controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::event.event');
+module.exports = createCoreController("api::event.event", ({ strapi }) => ({
+  // Get logged in users
+  async me(ctx) {
+    const user = ctx.state.user;
+    console.log("FOUND USER999= ", user);
+    if (!user) {
+      return ctx.badRequest(null, [
+        { messages: [{ id: "No authorization header was found" }] },
+      ]);
+    }  
+
+    const data = await strapi.db
+      .query("api::event.event")
+      .findMany({ where: { user: user.id } });
+
+    // const eventsByUser = result.toJSON();
+
+    console.log("FOUND DATA 11111= ", data);
+    if (!data) {
+      return ctx.notFound();
+    }
+
+    const sanitizedEntity = await sanitize.contentAPI.output(data);
+
+    return { data: sanitizedEntity };   
+  },
+}));
--- src/api/event/routes/event.js
+++ src/api/event/routes/event.js
@@ -5,5 +5,31 @@
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
+// const defaultRouter = createCoreRouter("api::event.event");
 
-module.exports = createCoreRouter('api::event.event');
+// const customRouter = (innerRouter, extraRoutes = []) => {
+//   let routes;
+//   return {
+//     get prefix() {
+//       return innerRouter.prefix;
+//     },
+//     get routes() {
+//       if (!routes) routes = innerRouter.routes.concat(extraRoutes);
+//       return routes;
+//     },
+//   };
+// };
+
+// const myExtraRoutes = [
+//   {
+//     method: "GET",
+//     path: "/api/events/me",
+//     handler: "api::event.event.me",
+//     config: {
+//       policies: [],
+//     },
+//   }
+// ];
+
+// module.exports = customRouter(...defaultRouter, myExtraRoutes);
+module.exports = createCoreRouter("api::event.event");
